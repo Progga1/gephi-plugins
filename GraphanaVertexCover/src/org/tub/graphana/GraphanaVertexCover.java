f2a0e7a897e7295befebe3ada5df71b68edb767b
Progga1 <fenderandreas@web.de>
2013-06-10 15:02:15 +0200

    fix project setup

---
+++ GraphanaVertexCover/src/org/tub/graphana/GraphanaVertexCover.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tub.graphana;
+
+import org.tub.akt.graphanaexecuter.GraphanaStatisticsBuilder;
+
+
+public class GraphanaVertexCover extends GraphanaStatisticsBuilder {
+    
+}
